37481cd91fa40b512db4164268edff5cc53f1eb2
kolowy <63844746+kolowy@users.noreply.github.com>
2021-03-18 18:59:42 +0100

    Update invite.js

--- commands/Connections/invite.js
+++ commands/Connections/invite.js
@@ -1,17 +1,13 @@
 const { MessageEmbed} = require("discord.js");
 
 module.exports.run = (client, message, args) => {
-
-  message.delete({ timeout: 1000 });
-
   const embed = new MessageEmbed()
     .setColor("YELLOW")
     .setAuthor(client.user.username)
-    .setTitle("__Pour ajouter le bot à votre serveur__ :aaa")
-    .setDescription("Cliquez [ici](https://discord.com/oauth2/authorize?client_id=816294305194573834&permissions=8&scope=bot) !")
-    .setFooter("Invit", "https://images.emojiterra.com/google/android-11/128px/2795.png")
+    .setTitle("__Pour ajouter le bot à votre serveur__ :")
+    .setDescription("[inviter le bot sur votre serveur !](https://discord.com/oauth2/authorize?client_id=816294305194573834&permissions=8&scope=bot)\n \n[Rejoindre notre serveur !](https://discord.gg/HuwK62QcFB)\n")
+    .setFooter("Invite", "https://images.emojiterra.com/google/android-11/128px/2795.png")
     .setTimestamp();
-
   message.channel.send(embed)
 };
 
