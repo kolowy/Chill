a34f2b3f2097675f084ac926688604fda155d07b
kolowy <63844746+kolowy@users.noreply.github.com>
2021-03-18 12:57:16 +0100

    Update purge.js

--- commands/Moderation/purge.js
+++ commands/Moderation/purge.js
@@ -5,13 +5,9 @@ module.exports.run = async (client, message, args, level, settings) => {
   message.delete({ timeout: 1000 });
 
   //NOTE perms
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`${message.author} - Vous devez avoir la permission **\`MANAGE_MESSAGES\`** pour utiliser la commande **\`purge\`**.`).then(msg => {
-    msg.delete({ timeout: 5000 });
-  });
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`${message.author} - Vous devez avoir la permission **\`MANAGE_MESSAGES\`** pour utiliser la commande **\`purge\`**.`)
 
-	if (isNaN(args[0]) || (args[0] < 1 || args[0] > 100)) return message.reply("Il faut spécifier un **nombre** entre 1 et 100 !").then(msg => {
-    msg.delete({ timeout: 5000 });
-  });
+	if (isNaN(args[0]) || (args[0] < 1 || args[0] > 100)) return message.reply("Il faut spécifier un **nombre** entre 1 et 100 !")
   
   const messages = await message.channel.messages.fetch({
     limit: Math.min(args[0], 100),
