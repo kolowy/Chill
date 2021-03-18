2f07fb56490a0e2837cee129016ab2667fe2b4d7
kolowy <63844746+kolowy@users.noreply.github.com>
2021-03-18 15:38:05 +0100

    Update minecraft.js

--- commands/Connections/minecraft.js
+++ commands/Connections/minecraft.js
@@ -3,57 +3,54 @@ const request = require('request');
 const mcPort = 25565;
 const color = "YELLOW"
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
+  ip = args[0]
 
-  message.delete({ timeout: 1000 });
-
-  let arg = message.content.split(" ").slice(1);
-  let echo = arg.join(" ")
-  if (arg.length > 0) {
-    message.delete();
-    var url = 'http://mcapi.us/server/status?ip=' + echo + '&port=' + mcPort;
-    request(url, function (err, response, body) {
-      if (err) {
-        const reportEmbed = new MessageEmbed()
-          .setTitle("Error", message.guild.iconURL)
-          .setDescription(" ")
-          .setColor(color)
-          .addField("Reason:", "Le serveur n'existe pas")
-          .setTimestamp()
-        return message.channel.send(reportEmbed);
-      }
-      body = JSON.parse(body);
-      if(body.online == false){
+  if (args.length > 1) {
+    mcPort = args[1]
+  }
+  var url = 'http://mcapi.us/server/status?ip=' + ip + '&port=' + mcPort;
+  request(url, function (err, response, body) {
+    if (err) {
+      const reportEmbed = new MessageEmbed()
+        .setTitle("Error", message.guild.iconURL)
+        .setDescription(" ")
+        .setColor(color)
+        .addField("Reason:", "Le serveur n'existe pas")
+        .setTimestamp()
+      return message.channel.send(reportEmbed);
+    }
+    body = JSON.parse(body);
+    if(body.online == false){
+      const errorEmbed = new MessageEmbed()
+        .setTitle("Error", message.guild.iconURL)
+        .setDescription(" ")
+        .setColor(color)
+        .addField("Reason:", `Le serveur **${ip}** n'existe pas, reverifiez votre ip..."`)
+        .setFooter("Minecraft",  "https://images.emojiterra.com/google/android-11/128px/1f50c.png")
+        .setTimestamp()
+      return message.channel.send(errorEmbed);
+    } else {
+      if (body.online) {
         const errorEmbed = new MessageEmbed()
           .setTitle("Error", message.guild.iconURL)
-          .setDescription(" ")
-          .setColor(color)
-          .addField("Reason:", "Le serveur n'existe pas, reverifiez votre ip...")
+          .setDescription(`Le serveur : **${ip}** est actuallement **online**.
+          Le nombre de personnes connecté est de : **${body.players.now}**`)
+          .setColor("GREEN")
           .setFooter("Minecraft",  "https://images.emojiterra.com/google/android-11/128px/1f50c.png")
           .setTimestamp();
         return message.channel.send(errorEmbed);
       } else {
-        if (body.online) {
-          const errorEmbed = new MessageEmbed()
-            .setTitle("Error", message.guild.iconURL)
-            .setDescription(`Le serveur : **${echo}** est actuallement **online**.
-            Le nombre de personnes connecté est de : **${body.players.now}**`)
-            .setColor("GREEN")
-            .setFooter("Minecraft",  "https://images.emojiterra.com/google/android-11/128px/1f50c.png")
-            .setTimestamp();
-          return message.channel.send(errorEmbed);
-        } else {
-          const errorEmbed = new MessageEmbed()
-            .setTitle("Error", message.guild.iconURL)
-            .setDescription(`Le serveur : **${echo}** est actuallement **offline**.`)
-            .setColor(color)
-            .setFooter("Minecraft",  "https://images.emojiterra.com/google/android-11/128px/1f50c.png")
-            .setTimestamp();
-          return message.channel.send(errorEmbed);
-        }
+        const errorEmbed = new MessageEmbed()
+          .setTitle("Error", message.guild.iconURL)
+          .setDescription(`Le serveur : **${ip}** est actuallement **offline**.`)
+          .setColor(color)
+          .setFooter("Minecraft",  "https://images.emojiterra.com/google/android-11/128px/1f50c.png")
+          .setTimestamp();
+        return message.channel.send(errorEmbed);
       }
-    });
-  }
+    }
+  });
 }
 
 module.exports.help = {
@@ -62,7 +59,7 @@ module.exports.help = {
   category: "connections",
   description: "Permet de savoir si un serveur est connecté.",
   cooldown: 10,
-  usage: "<server>",
+  usage: "<ip> <port>",
   permissions: false,
   args: true,
 }
