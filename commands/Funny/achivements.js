a295577df282ad9ccd246b7776614333dded571d
kolowy <63844746+kolowy@users.noreply.github.com>
2021-03-18 19:00:34 +0100

    Create achivements.js

---
+++ commands/Funny/achivements.js
@@ -0,0 +1,16 @@
+module.exports.run = (client, message, args) => {
+    message.delete({ timeout: 1000 });
+    msg = args.join("..").toLowerCase()
+    message.channel.send('https://minecraft-api.com/api/achivements/grass/' + msg )
+};
+
+module.exports.help = {
+  name: "achivements",
+  aliases: ["achi"],
+  category: "funny",
+  description: "Renvoi votre message sous forme d'achivement minecraft",
+  cooldown: 10,
+  usage: "<title>/<message>",
+  permissions: false,
+  args: true,
+}
