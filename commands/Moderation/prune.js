ddf868cb9ad22701e602672d2b2e117753a07dce
kolowy <63844746+kolowy@users.noreply.github.com>
2021-03-18 12:55:33 +0100

    Update prune.js

--- commands/Moderation/prune.js
+++ commands/Moderation/prune.js
@@ -5,32 +5,31 @@ module.exports.run = async (client, message, args, settings) => {
 
   message.delete({ timeout: 1000 });
 
+  //NOTE perms
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`${message.author} - Vous devez avoir la permission **\`MANAGE_MESSAGES\`** pour utiliser la commande .**\`prune\`**`);
 
   let user = message.guild.member(message.mentions.users.first());
   if (isNaN(args[1]) || (args[1] < 1 || args[1] > 100)) return message.reply("Il faut spécifier un **nombre** entre 1 et 100 !")
   
+  //NOTE filtre des msgs
   const messages = (await message.channel.messages.fetch({
     limit: 100,
     before: message.id
   })).filter(a => a.author.id === user.id).array();
 
   messages.length = Math.min(args[1], messages.length); 
-  
   if (messages.length === 0 || !user) return message.reply("Aucuns messages à supprimer pour cet utilisateur (ou celui-ci n'existe pas) !")
-
   if (messages.length === 1) await messages[0].delete();
   else await message.channel.bulkDelete(messages);
 
-  message.delete();
+  //TODO mettre via ddb logchannel
 
   const embed = new MessageEmbed()
     .setColor(color)
-    .setAuthor(`${user.username}\n(${user.id})`, user.avatarURL())
+    .setAuthor(`${message.author.username}\n(${message.author.id})`, message.author.avatarURL())
     .setDescription(`**Action :** \`PRUNE\`\n**Nbr de messages :** \`${args[1]}\`\n**Utilisateur :** \`${args[0]}\`\n**Salon :** \`${message.channel}\``)
     .setTimestamp()
     .setFooter(message.author.username, message.author.avatarURL());
-
     client.channels.cache.get(client.config.logChannel).send(embed);
 };
 
