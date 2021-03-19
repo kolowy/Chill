73c40e09f30a05c6b59276f034d631bfa86627a4
kolowy <63844746+kolowy@users.noreply.github.com>
2021-03-19 13:10:22 +0100

    Update ping.js

--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -3,16 +3,17 @@ const color = "RANDOM";
 
 module.exports.run = async (client, message, args) => {
 
-  message.delete({ timeout: 1000 });
+  message.delete({ timeout: 1000 }).catch((error) => {
+    message.channel.send('I cannot delete message here')
+  });
 
-   const msg = await message.channel.send("Ping !");
-
-   msg.edit(new MessageEmbed()
-      .setColor(color)
-      .setAuthor(message.author.username, message.author.avatarURL(), message.author.avatarURL())
-      .setTitle("Ping ! 🏓", message.guild.iconURL)
-      .setDescription(`Latence du message : \`${msg.createdTimestamp - message.createdTimestamp}ms\`\nLatence du BOT : \`${Math.round(client.ws.ping)}ms\``)
-    );
+  const msg = await message.channel.send("Ping !");
+  msg.edit(new MessageEmbed()
+    .setColor(color)
+    .setAuthor(message.author.username, message.author.avatarURL(), message.author.avatarURL())
+    .setTitle("Ping ! 🏓", message.guild.iconURL)
+    .setDescription(`Latence du message : \`${msg.createdTimestamp - message.createdTimestamp}ms\`\nLatence du BOT : \`${Math.round(client.ws.ping)}ms\``)
+  );
 };
 
 module.exports.help = {
